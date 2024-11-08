307b101f0d2073c3d4005b52d95d665e7e652c6c
Rob23oba <robin.arnez@web.de>
2024-11-08 15:14:14 +0100

    Add --compare mode for iwltas.cli.ViewLengths

--- clients/src/iwltas/cli/ViewLengths.java
+++ clients/src/iwltas/cli/ViewLengths.java
@@ -12,7 +12,9 @@ public class ViewLengths {
 		int depth = 1;
 		File dir = null;
 		File file = null;
+		File compareDir = null;
 		String expr = null;
+		boolean selfLength = false;
 		loop: for (int i = 0; i < args.length; i++) {
 			switch (args[i]) {
 			case "--help":
@@ -33,6 +35,14 @@ public class ViewLengths {
 			case "--recursive":
 				depth = -1;
 				break;
+			case "--compare":
+				if (i + 1 >= args.length) {
+					System.err.println("--compare requires a directory argument");
+					help = true;
+					break loop;
+				}
+				compareDir = new File(args[++i]);
+				break;
 			case "-e":
 				if (file != null) {
 					System.err.println("-e cannot be used when a file is provided");
@@ -45,6 +55,9 @@ public class ViewLengths {
 					break loop;
 				}
 				expr = args[++i];
+				break;
+			case "--self-length":
+				selfLength = true;
 				break;
 			default:
 				if (file != null) {
@@ -73,6 +86,8 @@ public class ViewLengths {
 			 --help:            Show this help message
 			 --hide-incomplete: Hide lengths of incomplete TASes (TASes that reference non-existent TASes)
 			 --lookup <dir>:    Use a different lookup directory
+			 --compare <dir>:   Compare results with a different lookup
+			 --self-length:     Only show the lengths of the TASes themselves without their references
 			 -e "...":          Show length of provided TAS
 			 -r, --recursive:   Traverse the entire file tree from the provided directory
 			""");
@@ -87,15 +102,21 @@ public class ViewLengths {
 			dir = new File(dir, "..");
 		}
 		TASLookup lookup = TASLookup.fromLookupDirectory(dir);
+		TASLookup compareLookup = compareDir != null ? TASLookup.fromLookupDirectory(compareDir) : null;
+		if (selfLength) {
+			lookup = name -> { throw new IOException(); };
+			compareLookup = name -> { throw new IOException(); };
+		}
+
 		List<Result> results = new ArrayList<>();
 		if (file != null) {
-			recurseCalculateLengths(file, "", depth, lookup, results);
+			recurseCalculateLengths(file, "", depth, lookup, compareDir, compareLookup, results);
 		}
 		if (expr != null) {
 			try (StringReader in = new StringReader(expr)) {
 				results.add(readerLength(expr, lookup, in));
 			} catch (IOException ex) {
-				results.add(new Result(expr, "error"));
+				results.add(new Result(expr, "error", 0));
 			}
 		}
 		int longestName = 0;
@@ -105,15 +126,75 @@ public class ViewLengths {
 			}
 			longestName = Math.max(longestName, r.fileName().length());
 		}
+		long prevCompare = 0L;
+		long diffTotal = 0L;
 		for (Result r : results) {
 			if (hide && r.report().endsWith("(incomplete)")) {
 				continue;
 			}
-			System.out.println(r.fileName() + " ".repeat(longestName + 4 - r.fileName().length()) + r.report());
+			String suffix = "";
+			if (compareDir != null && r.fileName().startsWith("a/")) {
+				prevCompare = r.length();
+			} else if (compareDir != null && r.fileName().startsWith("b/")) {
+				suffix = " (" + lengthDiff(r.length() - prevCompare) + ")";
+				diffTotal += r.length() - prevCompare;
+			}
+			System.out.println(r.fileName() + " ".repeat(longestName + 4 - r.fileName().length()) + r.report() + suffix);
+		}
+		if (compareDir != null) {
+			System.out.println("Total: " + lengthDiff(diffTotal));
 		}
 	}
 
-	public record Result(String fileName, String report) {
+	public static String lengthDiff(long diff) {
+		if (diff < 0) {
+			return diff + " frames";
+		} else if (diff > 0) {
+			return "+" + diff + " frames";
+		} else {
+			return "unchanged";
+		}
+	}
+
+	public record Result(String fileName, String report, long length) {
+	}
+
+	public static void recurseCalculateLengths(File file, String path, int depth, TASLookup lookup, File compareFile, TASLookup compareLookup, List<Result> out) {
+		if (file.isDirectory()) {
+			if (depth == 0) {
+				return;
+			}
+			if (compareFile != null && !compareFile.isDirectory()) {
+				return;
+			}
+			for (String s : file.list()) {
+				File f = null;
+				if (compareFile != null) {
+					f = new File(compareFile, s);
+					if (!f.exists()) {
+						continue;
+					}
+				}
+				recurseCalculateLengths(new File(file, s), path.isEmpty() ? s : path + "/" + s, depth < 0 ? depth : depth - 1, lookup, f, compareLookup, out);
+			}
+		} else if (file.getName().endsWith(".txt")) {
+			if (compareFile != null && !compareFile.isFile()) {
+				return;
+			}
+			String compPath = compareFile == null ? path : "a/" + path;
+			try (FileReader in = new FileReader(file)) {
+				out.add(readerLength(compPath, lookup, in));
+			} catch (IOException ex) {
+				out.add(new Result(compPath, "error", 0));
+			}
+			if (compareFile != null) {
+				try (FileReader in = new FileReader(compareFile)) {
+					out.add(readerLength("b/" + path, compareLookup, in));
+				} catch (IOException ex) {
+					out.add(new Result("b/" + path, "error", 0));
+				}
+			}
+		}
 	}
 
 	public static void recurseCalculateLengths(File file, String path, int depth, TASLookup lookup, List<Result> out) {
@@ -128,7 +209,7 @@ public class ViewLengths {
 			try (FileReader in = new FileReader(file)) {
 				out.add(readerLength(path, lookup, in));
 			} catch (IOException ex) {
-				out.add(new Result(path, "error"));
+				out.add(new Result(path, "error", 0));
 			}
 		}
 	}
@@ -141,7 +222,7 @@ public class ViewLengths {
 		if (incomplete[0]) {
 			result += " (incomplete)";
 		}
-		return new Result(path, result);
+		return new Result(path, result, length);
 	}
 
 	public static String twoDecimals(long number) {
